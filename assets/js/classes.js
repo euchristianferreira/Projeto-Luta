5ddc319f83ec742597ca37a7fde398a8c1b6257b
Christian Ferreira <christian.ferreira7146@hotmail.com>
2023-09-14 20:42:44 -0300

    Finalizando o projeto da Luta

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -30,7 +30,7 @@ class Knight extends Character {
     }
 }
 
-class Sorcerer extends Character{
+class Sorcerer extends Character {
     constructor(name) {
         super(name);
         this.life = 80;
@@ -40,7 +40,7 @@ class Sorcerer extends Character{
     }
 }
 
-class LittleMonster extends Character{
+class LittleMonster extends Character {
     constructor() {
         super('Little Monster');
         this.life = 40;
@@ -61,33 +61,71 @@ class BigMonster extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El,logObject) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObject;
     }
 
     start() {
         this.update();
-        
+
         this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
         this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
     }
 
     update() {
         // Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
 
         // Fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
     }
 
-    doAttack(attacking, attacked){
-        console.log(`${attacking.name} está atacando ${attacked.name}`);
+    doAttack(attacking, attacked) {
+        if (attacking.life <= 0 || attacked.life <= 0) {
+            this.log.addMessage('Esse cara já era!');
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if (actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            this.log.addMessage(`${attacking.name} Causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`);
+        } else {
+            this.log.addMessage(`${attacked.name} Conseguiu defender!`);
+        }
+
+        this.update();
+    }
+}
+
+class Log {
+    list = [];
+    constructor(listEl) {
+        this.listEl = listEl;
+    }
+
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+        for (let i in this.list) {
+            this.listEl.innerHTML += `<li>${this.list[i]}<li`;
+        }
     }
 }
